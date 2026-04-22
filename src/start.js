44e64cfb8e3d69fb26f85368198e0d1eea35ba6b
Erik Björnholm <eb225nm@student.lnu.se>
2026-04-22 15:26:08 +0200

    Fix tutorial 3 vite issues and es6 imports

--- src/start.js
+++ src/start.js
@@ -5,24 +5,19 @@ import { BattleScene } from './game/scenes/battle'
 import { UIScene } from './game/scenes/battle'
 
 var config = {
-    type: Phaser.AUTO,
-    parent: 'content',
-    width: 320,
-    height: 240,
-    zoom: 2,
-    pixelArt: true,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 0 },
-            debug: false // set to true to view zones
-        }
+  type: Phaser.AUTO,
+  parent: 'content',
+  width: 320,
+  height: 240,
+  zoom: 2,
+  pixelArt: true,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+      debug: false, // set to true to view zones
     },
-    scene: [
-        BootScene,
-        WorldScene,
-        BattleScene,
-        UIScene
-    ]
-};
-var game = new Phaser.Game(config);
+  },
+  scene: [BootScene, WorldScene, BattleScene, UIScene],
+}
+var game = new Phaser.Game(config)
