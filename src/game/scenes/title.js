817ac6980d9a7dfc8f9a587aecc330ab6ade6b2b
Erik Björnholm <eb225nm@student.lnu.se>
2026-04-27 16:41:03 +0200

    pull

---
+++ src/game/scenes/title.js
@@ -0,0 +1,48 @@
+import Phaser from 'phaser'
+
+export var TitleScene = new Phaser.Class({
+  Extends: Phaser.Scene,
+
+  initialize: function TitleScene() {
+    Phaser.Scene.call(this, { key: 'TitleScene' })
+  },
+
+  create: function () {
+    let bg = this.add.image(160, 120, 'title')
+    bg.setDisplaySize(320, 240)
+
+    this.add
+      .text(
+        160,
+        180,
+        'Use the arrow keys to move \n Z to confirm and X to cancel',
+        {
+          fontSize: '8px',
+          fontFamily: '"Press Start 2P", cursive',
+          fill: '#fff',
+          align: 'center',
+          stroke: '#000',
+          strokeThickness: 2,
+        }
+      )
+      .setOrigin(0.5)
+
+    this.add
+      .text(160, 220, 'Press Z to start the game', {
+        fontSize: '8px',
+        fontFamily: '"Press Start 2P", cursive',
+        fill: '#f5d400',
+        border: '1px solid black',
+        stroke: '#000',
+        strokeThickness: 2,
+      })
+      .setOrigin(0.5)
+
+    this.input.keyboard.on('keydown-Z', this.startGame, this)
+  },
+
+  // start the WorldScene
+  startGame: function () {
+    this.scene.start('WorldScene')
+  },
+})
